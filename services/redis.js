a097925a43eba43fa6e80dfcad9809cd970faf60
devgpb <bezerra.pereira.dev@gmail.com>
2025-08-29 00:01:32 -0300

    consertando redis

--- services/redis.js
+++ services/redis.js
@@ -1,5 +1,7 @@
 const { Redis } = require('ioredis');
 
-const connection = new Redis(process.env.REDIS_URL || 'redis://localhost:6379');
+const connection = new Redis(process.env.REDIS_URL || 'redis://localhost:6379', {
+  maxRetriesPerRequest: null,   // 👈 obrigatório para BullMQ
+});
 
 module.exports = connection;
